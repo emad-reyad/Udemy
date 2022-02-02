bb5787e0ac74c01f273a41092b133bddcaea3ccc
Emad Reyad <emreyad@moccae.gov.ae>
2022-02-02 23:02:29 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit{
+  constructor(private http:HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.GetUsers();
+  }
+  title = 'The Udemy App ';
+  Users:any;
+
+  GetUsers(){
+    this.http.get('https://localhost:5000/api/Customer').subscribe(
+    (response)=>{
+      this.Users=response
+    },
+    (error)=>{
+      console.log(error.message); 
+    });
+  }
+   
 }
+
+
